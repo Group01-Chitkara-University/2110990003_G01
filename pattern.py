a095e8e7e006ced48f84f64542a55e550d897d88
AahanWalia <aahan0003.be21@chitkara.edu.in>
2022-05-31 23:23:13 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,12 @@
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        x = 65
+x += 1
+        print("&#92r")
+    k = n - 2
+    x = 65
+pattern(5)
